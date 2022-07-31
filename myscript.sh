e4238ef445132550f5741fc6a958bb5b2723cf8c
bavinclint <bavinclint.com>
2022-07-31 14:27:57 +0300

    initial files

---
+++ myscript.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# ECHO COMMAND
+# echo Hello World!
+
+# VARIABLES
+# Uppercase by convention   
+# Letters, numbers, underscores
+NAME="Brad"
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
+
+# USER INPUT
+# read -p "Enter your name: " NAME
+# echo "Hello $NAME, nice to meet you!"
+
+# SIMPLE IF STATEMENT
+# if [ "$NAME" == "Brad" ]
+# then
+#  echo "Your name is Brad"
+# fi
